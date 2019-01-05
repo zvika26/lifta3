24ed0b6feb8bdd26184991642ccd3604ba57ed0e
vichter <zvi.vichter@microfocus.com>
2019-01-05 23:59:48 +0200

    2

---
+++ src/app/teachers/teachers-list/teachers-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teachers-list',
+  templateUrl: './teachers-list.component.html',
+  styleUrls: ['./teachers-list.component.sass']
+})
+export class TeachersListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
